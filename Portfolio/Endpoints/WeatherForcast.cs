92d93cfedcf05f8df38ace7bbce60d01d971ad74
Muhammad Aftab Hameed <muhammad.hameed@glaston.net>
2023-08-30 17:36:15 +0300

    # Project setup

--- Portfolio/Endpoints/WeatherForcast.cs
+++ Portfolio/Endpoints/WeatherForcast.cs
@@ -25,4 +25,9 @@
             .WithOpenApi();
         }
     }
+
+    internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
 }
--- Portfolio/Program.cs
+++ Portfolio/Program.cs
@@ -9,18 +9,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 app.RegisterEndpoints();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
