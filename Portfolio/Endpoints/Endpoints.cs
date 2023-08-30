f3ee312a689a8c0eb9426800a9b5c4e552b7d166
Muhammad Aftab Hameed <muhammad.hameed@glaston.net>
2023-08-30 17:13:51 +0300

    # Project setup

---
+++ Portfolio/Endpoints/Endpoints.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Endpoints
+{
+    public static class Endpoints
+    {
+        public static void RegisterEndpoints(this WebApplication app)
+        {
+            // Weather Forcast Endpoint
+            app.RegisterWeatherForecastEndpoint();
+
+        }
+    }
+}
